629b55eb07f3c829b121ab726331e02fc4057389
snmed <sandro@dallo.biz>
2020-11-22 13:40:13 +0100

    Fix linting complaints

--- doc.go
+++ doc.go
@@ -2,5 +2,4 @@
 Package cerberus is a Windows service helper program inspired by [NSSM](https://nssm.cc/).
 It can be used to create and manage Windows services for ordinary binaries.
 */
-
 package cerberus
